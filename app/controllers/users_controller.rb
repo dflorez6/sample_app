18e45a23711f9c1ce95afa80b47d60cd6b4fdc73
David Florez <dflorez6@gmail.com>
2013-07-04 03:54:00 -0500

    CH 7 Done!!!!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def new
+  def show                          # Show action
+    @user = User.find(params[:id])
+  end
+
+  def new                           # New action
+    @user = User.new
+  end
+
+  def create                        # Create action
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"  # Flash message if signup is successful
+      redirect_to @user             # Redirects to the user's show page
+    else
+      render 'new'                  # Renders "new" view with error message when signup fails due to invalid info
+    end
   end
 end
